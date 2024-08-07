4d62242a6e0e296c7cabb0d96efd45cb187c26e0
naveen9979 <sathyapriyak2020@gmail.com>
2024-08-06 22:54:53 -0700

    Add files via upload

---
+++ Bitwise.java
@@ -0,0 +1,32 @@
+    public class Bitwise{
+    public static void main(String[] args) {
+        int aa = 78;
+        int bb = 34;
+        System.out.println(aa & bb);
+
+
+
+        int n =677;
+        int s =999;
+        System.out.println(n | s);
+
+
+        int p = 998;
+        int c = 676;
+        System.out.println(p ^ c);
+
+        int j = 987;
+        int x = 876;
+        System.out.println(x << j);
+        System.out.println(j >> x);
+
+
+        int hh = 87;
+        int uu = 76;
+        System.out.println(hh >>> uu);
+
+    }
+    
+} 
+    
+
